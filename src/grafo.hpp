49a7c060b018fc283b53c5c8f5ebc36959d6def7
Sleal11 <santiagoald0203@gmail.com>
2025-11-26 23:50:54 -0400

    Add Grafo class and NodoLista struct definition

--- src/grafo.hpp
+++ src/grafo.hpp
@@ -1,1 +1,31 @@
+#ifndef GRAFO_HPP
+#define GRAFO_HPP
 
+#include <iostream>
+using namespace std;
+
+struct NodoLista {
+    int destino;
+    int peso;
+    NodoLista* siguiente;
+    NodoLista(int d, int p);
+};
+
+class Grafo {
+private:
+    int cantidadNodos;
+    NodoLista** listaAdyacencia;
+
+public:
+    Grafo(int cantidad);
+    ~Grafo();
+
+    void agregarArista(int origen, int destino, int peso = 1);
+    void recorridoAnchura(int inicio);
+    void recorridoProfundidad(int inicio);
+    void recorridoProfundidadRecursivo(int nodo, bool* visitado);
+    void caminoMasCorto(int inicio);
+    void cargarDesdeCSV(const char* archivoCSV);
+};
+
+#endif
